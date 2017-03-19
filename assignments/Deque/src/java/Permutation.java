20878fce18a460b17c268865b32d5e177c7b570e
tong su <tong_su@hotmail.com>
2017-03-19 13:32:04 -0400

    read input from console

--- assignments/Deque/src/java/Permutation.java
+++ assignments/Deque/src/java/Permutation.java
@@ -1,22 +1,18 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.util.NoSuchElementException;
-
 public class Permutation {
-    public static void main(String[] args){
-        if(args.length != 1){
+    public static void main(String[] args) {
+        if (args.length != 1) {
             throw new IllegalArgumentException("must provide permutation size");
         }
 
         final Integer size = Integer.valueOf(args[0]);
 
         RandomizedQueue<String> items = new RandomizedQueue<>();
-        try{
-            final String item = StdIn.readString();
-            items.enqueue(item);
-        } catch (NoSuchElementException e){
-
+        final String[] input = StdIn.readAllStrings();
+        for (String s : input) {
+            items.enqueue(s);
         }
 
         for (int i = 0; i < size; i++) {
